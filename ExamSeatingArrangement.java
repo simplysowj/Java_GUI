1065fbe9a5460ac6cf0b042a7551edba02c99a57
sowjanya <90475451+simplysowj@users.noreply.github.com>
2023-12-14 21:57:34 -0500

    Add files via upload

---
+++ ExamSeatingArrangement.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.util.Vector;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ExamSeatingArrangement {
+    JFrame seatFrame=new JFrame();
+    private JPanel frame;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+    private JTable table1;
+    private JLabel Name;
+    private JLabel Rollno;
+    private JLabel classDetail;
+    private JButton GETSEATButton;
+}
